643df2cdac08bdac50e98a709b762120aadf2469
Matheus Bloize <mbloizepd@gmail.com>
2023-11-10 12:29:28 -0300

    feat: custom student page

--- src/app/student/[id]/page.tsx
+++ src/app/student/[id]/page.tsx
@@ -2,21 +2,82 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 import checkImage from "@/assets/images/check.svg";
 import returnImage from "@/assets/images/return.svg";
 import Subject from "@/components/Subject";
+import {
+  student1,
+  student2,
+  student3,
+  student4,
+  student5,
+  student6,
+  student7,
+  student8,
+  student9,
+  student10,
+  StudentInterface,
+} from "@/data/students";
 
 const Student = () => {
+  const [student, setStudent] = useState<StudentInterface>();
   const params = useParams();
+
+  const findStudent = useCallback(() => {
+    switch (params.id) {
+      case "1":
+        setStudent(student1);
+        break;
+      case "2":
+        setStudent(student2);
+        break;
+      case "3":
+        setStudent(student3);
+        break;
+      case "4":
+        setStudent(student4);
+        break;
+      case "5":
+        setStudent(student5);
+        break;
+      case "6":
+        setStudent(student6);
+        break;
+      case "7":
+        setStudent(student7);
+        break;
+      case "8":
+        setStudent(student8);
+        break;
+      case "9":
+        setStudent(student9);
+        break;
+      case "10":
+        setStudent(student10);
+        break;
+      default:
+        setStudent({
+          name: "Student Not Found",
+          age: 999,
+          subject: "nothing",
+        });
+        redirect("/");
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    findStudent();
+  }, [findStudent, params.id]);
 
   return (
     <main className="w-[85%] mx-auto flex-1 flex flex-col items-center">
-      <section className="flex w-full gap-[2em] mt-[2em] max-md:flex-col">
+      <section className="flex w-full gap-[2em] mt-[2em] max-md:flex-col max-md:items-center">
         <div className="border-solid border-[.313em] border-[#010101] rounded-[.625em] w-fit">
           <Image
-            className="rounded-[.25em]"
+            className="rounded-[.25em] h-full"
             src={`/static/images/student${params.id}.jpg`}
             alt="std"
             width={300}
@@ -25,18 +86,25 @@ const Student = () => {
         </div>
         <section className="flex flex-col justify-between">
           <article>
-            <h3 className="text-[2em] font-bold">Jane Doe</h3>
-            <span className="text-[1.75em]">18 years old</span>
-            <p>Likes to study science</p>
+            {student && (
+              <>
+                <h3 className="text-[2em] font-bold">{student.name}</h3>
+                <span className="text-[1.75em]">{student.age} years old</span>
+                <p>Likes to study {student.subject}</p>
+              </>
+            )}
           </article>
           <article className="flex flex-col items-center max-md:my-[2em]">
             <span>Subjects</span>
-            <section className="grid grid-cols-6 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 gap-[1em] w-full mt-[1em]">
+            <section
+              id="grades-section"
+              className="grid grid-cols-6 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 gap-[1em] w-full mt-[1em] place-items-center"
+            >
               <Subject name="Science" />
               <Subject name="Math" />
               <Subject name="English" />
               <Subject name="P.E." />
-              <Subject name="Economy" />
+              <Subject name="Finance" />
               <Subject name="History" />
             </section>
           </article>
@@ -51,6 +119,7 @@ const Student = () => {
           />
         </Link>
         <Image
+          tabIndex={0}
           className="transition-all cursor-pointer hover:scale-[1.1] hover:mix-blend-color-burn"
           src={checkImage}
           alt="check"
---
+++ src/data/students.ts
@@ -0,0 +1,65 @@
+export interface StudentInterface {
+  name: string;
+  age: number;
+  subject: string;
+}
+
+export const student1 = {
+  name: "Eddie Scott",
+  age: 15,
+  subject: "english",
+};
+
+export const student2 = {
+  name: "Robert Jones",
+  age: 17,
+  subject: "science",
+};
+
+export const student3 = {
+  name: "Charlotte Smith",
+  age: 16,
+  subject: "history",
+};
+
+export const student4 = {
+  name: "Emma Miller",
+  age: 16,
+  subject: "math",
+};
+
+export const student5 = {
+  name: "Chloe Johnson",
+  age: 15,
+  subject: "finance",
+};
+
+export const student6 = {
+  name: "Chris Burton",
+  age: 16,
+  subject: "history",
+};
+
+export const student7 = {
+  name: "Michael Harris",
+  age: 16,
+  subject: "science",
+};
+
+export const student8 = {
+  name: "Ashley Parker",
+  age: 16,
+  subject: "science",
+};
+
+export const student9 = {
+  name: "Thomas Wilson",
+  age: 17,
+  subject: "math",
+};
+
+export const student10 = {
+  name: "Mary Jackson",
+  age: 16,
+  subject: "math",
+};
